dec467fb796174164b0ac90cd9f66c1d2c842c25
mit <nguyenhuutien.hcmut.edu.vn@gmail.com>
2024-03-15 00:17:56 +0900

    Third commit

--- Hello.py
+++ Hello.py
@@ -1,2 +1,6 @@
 print("Hello World")
 print("Hello Son. I love you so much")
+message="Hello Son, MIT. Welcome to farmily"
+print(message)
+message="Hello Son, We are very love you, MIT"
+print(message)
